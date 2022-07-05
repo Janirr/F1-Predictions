8900fbf947e48be8fc99e368ea6ed77d532138ff
Jakub Januszewski <janir3g@gmail.com>
2022-07-05 16:09:00 +0200

    Added radio buttons for making predictions

--- resources/views/predictions/create.blade.php
+++ resources/views/predictions/create.blade.php
@@ -5,61 +5,35 @@
         <h1 class="text-5xl uppercase bold">Make a prediction!</h1>
         </div>
     </div>
-
+    
     <div class="flex justify-center pt-20">
         <form action="/predictions" method="POST">
             @csrf
-            <div class="block">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="first" 
-                    placeholder="P1">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="second" 
-                    placeholder="P2">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="third" 
-                    placeholder="P3">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="fourth" 
-                    placeholder="P4">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="fifth" 
-                    placeholder="P5">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="sixth" 
-                    placeholder="P6">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="seventh" 
-                    placeholder="P7">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="eighth" 
-                    placeholder="P8">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="nineth" 
-                    placeholder="P9">
-                    <input 
-                    type="text" 
-                    class="block shadow-5xl mb-10 p-2 w-80 italic placeholder-gray-400" 
-                    name="tenth" 
-                    placeholder="P10">
+            <div class="block align-items-center float-left">
+                @php
+                $listOfDrivers=['Max Verstappen','Sergio Pérez','Charles Leclerc','George Russell','Carlos Sainz Jr.','Lewis Hamilton','Lando Norris','Valtteri Bottas','Esteban Ocon','Fernando Alonso','Pierre Gasly','Kevin Magnussen','Daniel Ricciardo','Sebastian Vettel','Yūki Tsunoda','Zhou Guanyu','Alexander Albon','Lance Stroll','Mick Schumacher','Nico Hülkenberg','Nicholas Latifi'];
+                $names=['first','second','third','fourth','fifth','sixth','seventh','eighth','nineth','tenth'];
+                @endphp
+                @foreach ($names as $name)
+                <span class="float-left">
+                    <h2 class="text-green-600 text-2xl font-bold text-center">
+                        {{ strtoupper($name) }}
+                    </h2>
+                    @foreach ($listOfDrivers as $driver)
+                        <input 
+                        type="radio" 
+                        class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer" 
+                        name="{{ $name }}"
+                        id="{{ $name }}{{ $driver }}" 
+                        value="{{ $driver }}">
+                        <label class="form-check-label inline-block text-gray-800" for="{{ $name }}{{ $driver }}">
+                            {{ $driver }}
+                        </label>
+                        <br>
+                    @endforeach
+                    </span>
+                @endforeach
+                
                 <button type="submit" class="bg-green-500 block shadow-5xl mb-10 p-2 w-80 uppercase font-bold">Submit</button>
             </div>
         </form>
